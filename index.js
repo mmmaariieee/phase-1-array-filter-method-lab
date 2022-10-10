3a46addaab08be11441dca955bddc050549b2741
Mariia <m.goncharenkoo@icloud.com>
2022-10-10 16:25:50 -0400

    commit tree 66884fd263b1fa920c4c0306697a98f9f70f1a6f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+// function findMatching(array, name) {
+//     return array.indexof(`${name}`)
+// }
+
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+}
+
+function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+}
+
+function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
